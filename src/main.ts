976f69440456a6be210128a9f0cbcfdef24f8e5c
Nathan Geronimo <nathangero@yahoo.com>
2025-03-04 13:17:53 -0800

    implement saving and loading tasks

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 const list = document.querySelector<HTMLUListElement>("#list")
 const form = document.querySelector<HTMLFormElement>("#new-task-form")
 const input = document.querySelector<HTMLInputElement>("#new-task-title")
+const tasks: Task[] = loadTasks()
+tasks.forEach(addListItem)
 
 form?.addEventListener("submit", event => {
   event.preventDefault();
@@ -36,16 +38,37 @@ form?.addEventListener("submit", event => {
   }
 
   addListItem(newTask)
+  tasks.push(newTask);
+  saveTasks()
   input.value = "";
 })
+
 
 function addListItem(task: Task) {
   const item = document.createElement("li");
   const label = document.createElement("label");
-  const checkbox = document.createElement("input")
-  checkbox.type = "checkbox"
+  const checkbox = document.createElement("input");
+  checkbox.addEventListener("change", () => {
+    task.completed = checkbox.checked
+    saveTasks()
+  });
+  checkbox.type = "checkbox";
+  checkbox.checked = task.completed;
 
   label.append(checkbox, task.title);
   item.append(label);
   list?.append(item);
+}
+
+function saveTasks() {
+  localStorage.setItem("TASKS", JSON.stringify(tasks));
+}
+
+function loadTasks(): Task[] {
+  const tasks = localStorage.getItem("TASKS");
+
+  // If no tasks, return an empty array
+  if (tasks == null) return []
+
+  return JSON.parse(tasks);
 }
