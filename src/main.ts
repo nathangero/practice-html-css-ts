e806be3ede7184a34995875c2b8338677df573f9
Nathan Geronimo <nathangero@yahoo.com>
2025-03-03 16:12:01 -0800

    implement adding a task to the list

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
 import './style.css'
+import { v4 as uuidV4 } from "uuid";
+
+type Task = {
+  id: string
+  title: string
+  completed: boolean
+  createdAt: Date
+}
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -10,3 +18,34 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   </div>
 `
 
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.querySelector<HTMLFormElement>("#new-task-form")
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+form?.addEventListener("submit", event => {
+  event.preventDefault();
+
+  // Make sure the input isn't empty
+  if (input?.value == "" || input?.value == null) return
+
+  const newTask: Task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  addListItem(newTask)
+  input.value = "";
+})
+
+function addListItem(task: Task) {
+  const item = document.createElement("li");
+  const label = document.createElement("label");
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+}
